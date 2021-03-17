bb5ebd4c103b1c2a679883b5af9b6a20a14f5324
Samapti Nag Bindu <jagadbandhu22@gmail.com>
2021-03-17 05:16:52 -0600

    first commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 const Cart = (props) => {
     const cart = props.cart;
+    // console.log(cart);
     // const total = cart.reduce((total, prd) => total + prd.price, 0);
     let total = 0;
-    for(let i = 0; i< cart.length; i++){
+    for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
+        debugger;
     }
 
     let shipping = 0;
-    if(total > 35){
+    if (total > 35) {
         shipping = 0;
     }
-    else if(total > 15){
+    else if (total > 15) {
         shipping = 4.99;
     }
-    else if(total > 0){
+    else if (total > 0) {
         shipping = 12.99
     }
 
@@ -29,12 +31,16 @@ const Cart = (props) => {
     }
     return (
         <div>
-            <h4>Order Summary</h4>
+            <h4 className="text-primary">Order Summary</h4>
             <p>Item Ordered: {cart.length}</p>
             <p>Product Price: {formatNumber(total)}</p>
             <p><small>shipping Cost: {shipping}</small></p>
             <p><small>Tax + VAT: {tax}</small></p>
             <p>Total Price: {grandTotal}</p>
+            <br />
+            {
+                props.children
+            }
         </div>
     );
 };
---
+++ src/components/Login/Login.js
@@ -0,0 +1,124 @@
+import { useContext, useState } from 'react';
+import { UserContext } from "../../App";
+import { useHistory, useLocation } from "react-router";
+import { createUserWithEmailAndPassword, handleFbSignIn, handleGoogleSignIn, handleSignOut, initializeLoginFramework, signInWithEmailAndPassword } from './loginManager';
+
+
+function Login() {
+    const [newUser, setNewUser] = useState(false);
+    const [user, setUser] = useState({
+        isSignedIn: false,
+        name: '',
+        email: '',
+        password: '',
+        photoURL: ''
+    });
+
+    initializeLoginFramework();
+
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+
+    const googleSignIn = () => {
+        handleGoogleSignIn()
+            .then(res => {
+                handleResponse(res, true);
+            })
+    }
+
+    const fbSignIn = () => {
+        handleFbSignIn()
+            .then(res => {
+                handleResponse(res, true);
+            })
+    }
+
+    const signOut = () => {
+        handleSignOut()
+            .then(res => {
+                handleResponse(res, false);
+            })
+    }
+
+    const handleResponse = (res, redirect) => {
+        setUser(res);
+        setLoggedInUser(res);
+        if(redirect){
+            history.replace(from);
+        }
+    }
+
+    const handleChange = (e) => {
+        let isFieldValid = true;
+        if (e.target.name === 'email') {
+            isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+        }
+        if (e.target.name === 'password') {
+            const isPasswordValid = e.target.value.length > 6;
+            const passwordHasNumber = /\d{1}/.test(e.target.value);
+            isFieldValid = isPasswordValid && passwordHasNumber;
+        }
+        if (isFieldValid) {
+            const newUserInfo = { ...user };
+            newUserInfo[e.target.name] = e.target.value;
+            setUser(newUserInfo);
+        }
+    }
+    const handleSubmit = (e) => {
+        if (newUser && user.email && user.password) {
+            createUserWithEmailAndPassword(user.name, user.email, user.password)
+                .then(res => {
+                    handleResponse(res, true);
+                })
+        }
+
+        if (!newUser && user.email && user.password) {
+            signInWithEmailAndPassword(user.email, user.password)
+                .then(res => {
+                    handleResponse(res, true);
+                })
+        }
+        e.preventDefault();
+    }
+
+
+    return (
+        <div style={{ textAlign: 'center' }}>
+            {
+                user.isSignedIn ? <button onClick={signOut}>Sign out</button> :
+                    <button variant="primary" onClick={googleSignIn}>Sign in</button>
+            }
+            <br />
+            <br />
+            <button onClick={fbSignIn}>Sign in using Facebook</button>
+            {
+                user.isSignedIn &&
+                <div>
+                    <p>Welcome, {user.name}!</p>
+                    <p>Your email: {user.email}</p>
+                    <img src="{user.photo}" alt="" />
+                </div>
+            }
+
+            <h1>Our own Authentication</h1>
+            <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
+            <label htmlFor="newUser">New User Sign up</label>
+            <form onSubmit={handleSubmit}>
+                {newUser && <input name="name" type="text" onBlur={handleChange} placeholder="Your name" />}
+                <br />
+                <input type="text" name="email" onBlur={handleChange} placeholder="Your email address" required />
+                <br />
+                <input type="password" name="password" onBlur={handleChange} placeholder="Type Your password" required />
+                <br />
+                <input type="submit" value={newUser ? 'Sign up' : 'Sign in'} />
+            </form>
+            <p style={{ color: 'red' }}>{user.error}</p>
+            { user.success && <p style={{ color: 'green' }}>User {newUser ? 'created' : 'Logged In'} successfully</p>}
+        </div>
+    );
+}
+
+export default Login;
